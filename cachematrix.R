1b6f50117d932c0d527004ae4a1570ab199079dd
Uberzaki <mc_mahon@hotmail.co.uk>
2015-08-23 11:01:01 +0100

    Created some comments and a bareboned application wich is essentially a refactored version makeVector. makeCacheMatrix currently does not return the list with environment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
 
-## Write a short comment describing this function
+## Constructs a matrix in an environment containg auxillary variables which cache a mean value 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		inverse <- NULL	
+	}
+	get <- function() x
+	setinverse <- function(set) inverse <<- set
+	getinverse <- function() inverse
 }
 
 
